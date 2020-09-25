27ebcae01a224529f5485d06e159b1bd26a4211c
Johel <49104482+JohelPF@users.noreply.github.com>
2020-09-24 18:57:27 -0600

    Camara v3.0

---
+++ Assets/Boundaries.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.234883f, 1.234883f),
+            Mathf.Clamp(transform.position.y, -0.4622732f, 0.4622732f), transform.position.z);
+    }
+}
